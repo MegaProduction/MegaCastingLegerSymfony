1b27eca8cb9a985fd195118c10cc652bd23663b6
Damien Buchet <damienbuchetdb@gmail.com>
2020-11-23 10:47:31 +0100

    Edit files

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -19,5 +21,28 @@ class ContactController extends AbstractController
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
+    }
+    public function new(Request $request): Response
+    {
+        // creates a task object and initializes some data for this example
+        $task = new Task();
+        $task->setTask('Prénom');
+        $task->setTask('Nom');
+        $task->setTask('E-mail');
+        $task->setTask('Adresse');
+        $task->setTask('Objet');
+        $task->setTask('Message');
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('task', TextType::class)
+            ->add('task', TextType::class)
+            ->add('task', TextType::class)
+            ->add('task', ChoiceType::class)
+            ->add('task', TextareaType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        // ...
     }
 }
