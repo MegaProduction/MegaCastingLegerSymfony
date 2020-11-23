2ab6a4eb7da7362d4e4b3e69654167087cc03dbf
Damien Buchet <damienbuchetdb@gmail.com>
2020-11-23 09:46:53 +0100

    Initial commit

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
