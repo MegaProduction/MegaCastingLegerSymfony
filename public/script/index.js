a62ebeaac7e455a4bc6ee0d8d85e32d6cb4ca8f3
Alexandre BREAULT <a.breault@iia-laval.fr>
2021-03-22 22:59:45 +0100

    edit script active link

--- public/script/index.js
+++ public/script/index.js
@@ -1,9 +1,5 @@
 let link = location.pathname
 let select = link.substring(1)
-let elementactive = $('#' + select);
-let activeLink =$('.active');
-console.log(activeLink);
-if (!elementactive.hasClass("active")) {
-    activeLink.removeClass("active");
-    elementactive.addClass("active");
-}
+let activeLink = $('.active');
+$('.active').removeClass('active');
+$('#' + select).addClass('active');
